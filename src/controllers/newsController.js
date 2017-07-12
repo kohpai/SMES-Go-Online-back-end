dbf156375825c4e7cb81b4eab02fbf6b824ac7c5
Bass <programmerfirst@hotmail.com>
2017-07-12 22:28:41 +0700

    _ news paging

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -14,13 +14,39 @@ import { Util, Enum } from '../helper'
    - catch error
    */
 router.route('/').get((req, res, next) => {
+
+    var page = parseInt(req.query.page, 0)
+    var limit = parseInt(req.query.limit, 0)
+
+    var send = {
+        status: Enum.res_type.FAILURE,
+        info: {}
+    };
+
+    NewsModel.getNews(page*limit, limit, (news) => {
+        if (news instanceof Error) {
+            send.message = 'Error getting news';
+            send.hint = news.sqlMessage;
+
+            return res.json(send);
+        }
+
+        send.status = Enum.res_type.SUCCESS;
+        send.info = news;
+
+        return res.json(send);
+
+    });
+})
+
+router.route('/count').get((req, res, next) => {
     // try {
     var send = {
         status: Enum.res_type.FAILURE,
         info: {}
     };
 
-    NewsModel.getNews((news) => {
+    NewsModel.countNews((news) => {
         if (news instanceof Error) {
             send.message = 'Error getting news';
             send.hint = news.sqlMessage;
--- src/models/newsModel.js
+++ src/models/newsModel.js
@@ -6,9 +6,25 @@ import DB from '../db.js'
 
 const timeout = 20000;
 
-const getNews = (done) => {
+const getNews = (offset, limit, done) => {
     var queryOption = {
-        sql: 'SELECT * FROM `news`;',
+        sql: 'SELECT * FROM `news` LIMIT ? OFFSET ?;',
+        timeout: timeout, // 40s
+        values: [limit, offset],
+    };
+
+    DB.get().query(queryOption, function(error, results, fields) {
+        if (error) {
+            return done(error);
+        } else {
+            return done(results);
+        }
+    });
+}
+
+const countNews = (done) => {
+    var queryOption = {
+        sql: 'SELECT COUNT(*) AS count FROM `news`;',
         timeout: timeout, // 40s
     };
 
@@ -26,5 +42,6 @@ const getNews = (done) => {
 }
 
 export default {
-    getNews
+    getNews,
+    countNews
 }
